bea7e04487565f52eaa04341905bae106e85dcce
Farnam Homayounfard <farnamhomayounfard@farnams-mbp.home>
2023-10-22 22:35:47 +0200

    refactor: Clean up code and improve readability

--- bot.py
+++ bot.py
@@ -1,97 +1,133 @@
 import os
+import random
 
 import telebot
 import requests
 import json
 import time
-from telebot import types
 from dotenv import load_dotenv
-import random
 from time import sleep
+import logging
 
+from settings import city_api_map
+from utils import fetch_ind_dates, send_to_chat
+
+# Load environment variables
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 IND_API_URL = os.getenv('IND_API_URL')
+ADMIN_ID = os.getenv('ADMIN_ID')
 
+# Initialize bot and logger
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+# logger = telebot.logger
+# telebot.logger.setLevel(logging.DEBUG)
 
-MAX_RETRIES = 5
-SLEEP_TIME = 2  # Time to sleep between retries, in seconds
+# In-memory storage for user settings. For production, consider using a database.
+user_settings = {}
+user_stop_flags = {}
 
+# delete commands for adminid
+bot.delete_my_commands(scope=telebot.types.BotCommandScopeAllChatAdministrators())
+bot.delete_my_commands(scope=telebot.types.BotCommandScopeAllPrivateChats())
+bot.delete_my_commands(scope=telebot.types.BotCommandScopeChat(ADMIN_ID))
+bot.delete_my_commands(scope=telebot.types.BotCommandScopeDefault())
 
-def fetch_ind_dates():
-    retries = 0
-    while retries < MAX_RETRIES:
-        try:
-            response = requests.get(IND_API_URL)
-            response.raise_for_status()
-
-            if not response.text:
-                print("Empty response received from API.")
-                return None
-
-            cleaned_response_text = response.text[5:]  # Remove first 5 characters
-            slots = json.loads(cleaned_response_text)
-
-            # Organize slots by date
-            slots_by_date = {}
-            for slot in slots['data']:
-                date = slot['date']
-                if date not in slots_by_date:
-                    slots_by_date[date] = []
-                slots_by_date[date].append(slot)
-
-            # Sort dates and get time slots for the first 5 days
-            sorted_dates = sorted(slots_by_date.keys())[:5]
-            earliest_slots = {date: slots_by_date[date] for date in sorted_dates}
-
-            return earliest_slots
-
-        except requests.RequestException as e:
-            print(f"Error occurred while calling API: {e}")
-            return None
-        except json.JSONDecodeError as e:
-            print(f"Failed to decode JSON response: {e}, Response Content: {response.text}")
-            return None
-
-    retries += 1
-    sleep_time = SLEEP_TIME + random.uniform(0, 1)  # Adding some jitter
-    print(f"Retrying in {sleep_time:.2f} seconds...")
-    sleep(sleep_time)
-
-
-def send_to_channel(slots_by_date):
-    for date, slots in slots_by_date.items():
-        slot_times = ["{0} - {1}".format(slot['startTime'], slot['endTime']) for slot in slots]
-
-        message = (
-            "📅 Date: {0}\n"
-            "🕒 Available Time Slots:\n\n{1}"
-            "\n\nGet your appointment: https://oap.ind.nl/oap/en/#/BIO"
-        ).format(date, '\n'.join(slot_times))
-
-        bot.send_message(CHANNEL_ID, message)
+bot.set_my_commands(commands=[
+    telebot.types.BotCommand("start", "Start the bot"),
+    telebot.types.BotCommand("settings", "Change settings"),
+    telebot.types.BotCommand("help", "About the bot"),
+], scope=telebot.types.BotCommandScopeDefault())
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text="Fetch Slots", callback_data="start_monitoring")
+def settings(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for city in city_api_map.keys():
+        # Convert 'the_hague' to 'The Hague'
+        city_label = city.replace('_', ' ').capitalize()
+        button = telebot.types.InlineKeyboardButton(
+            text=city_label, callback_data=f"set_city_{city}")
+        keyboard.add(button)
+    bot.send_message(message.chat.id, "Choose a city:", reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['help'])
+def helpcommand(message):
+    bot.send_message(message.chat.id, "This bot fetches IND appointment slots for you. "
+                                      "You can set a city and start fetching data. \n"
+                                      "You will be notified when slots are available each 1 minute. "
+                                      "To start, use the /start command.")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('set_city_'))
+def set_city(call):
+    city = call.data[len('set_city_'):]
+    user_settings[call.from_user.id] = city
+    bot.send_message(call.message.chat.id, f"City set to {city.capitalize()}.")
+
+    # Ask the user if they want to start fetching data
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(
+        text="Start fetching data", callback_data="start_fetching")
     keyboard.add(button)
-    bot.send_message(message.chat.id, "Please choose:", reply_markup=keyboard)
+    bot.send_message(call.message.chat.id,
+                     "Do you want to start fetching data?", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.data == "start_monitoring":
-        bot.send_message(call.message.chat.id, "Starting to monitor IND API...")
-        while True:
-            slots_by_date = fetch_ind_dates()
-            if slots_by_date:
-                send_to_channel(slots_by_date)
-            time.sleep(300)  # Wait for 5 minutes before the next API call
+@bot.callback_query_handler(func=lambda call: call.data == "start_fetching")
+def start_fetching(call):
+    user_id = call.from_user.id
+    chat_id = call.message.chat.id
+    bot.send_message(chat_id, "Starting to fetch data...")
 
+    # Initialize or reset the stop flag for this user
+    user_stop_flags[user_id] = False
+
+    if user_id in user_settings:
+        city = user_settings[user_id]
+        api_url = city_api_map.get(city)
+
+        if api_url:
+
+            while True:
+                # Check if the user has requested to stop
+                if user_stop_flags.get(user_id, True):
+                    print(f"Stopped fetching data for user {user_id}")
+                    break
+
+                slots_by_date = fetch_ind_dates(api_url)
+
+                if slots_by_date:
+                    send_to_chat(bot, slots_by_date, chat_id)
+
+                    # After fetching and sending the data, send the stop button
+                    keyboard = telebot.types.InlineKeyboardMarkup()
+                    button = telebot.types.InlineKeyboardButton(
+                        text="Stop fetching data", callback_data="stop_fetching")
+                    keyboard.add(button)
+                    bot.send_message(chat_id, "Click below to stop fetching data:", reply_markup=keyboard)
+
+                elif slots_by_date is None or len(slots_by_date) == 0:
+                    bot.send_message(chat_id, "No available dates.")
+                    break
+                time.sleep(60)
+
+    else:
+        bot.send_message(chat_id, "Please set a city using the /start command.")
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "stop_fetching")
+def stop_fetching(call):
+    user_id = call.from_user.id
+    user_stop_flags[user_id] = True  # Set the stop flag for this user
+    bot.send_message(call.message.chat.id,
+                     "Stopping data fetch. You can restart anytime by /start.")
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
 
 bot.polling(non_stop=True)
